ab44cdc145eb38ba15f6c25f5cc278ee0c284d36
EngineMachiner <mfpc98@gmail.com>
2023-12-04 13:25:45 -0500

    add: ThemeProvider material UI

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import type { AppProps } from 'next/app'
+
+import { lightTheme } from '../themes'
+
+export default function App( { Component, pageProps }: AppProps ) {
+
+    return ( 
+        <ThemeProvider theme={ lightTheme }>
+            <CssBaseline/>
+            <Component { ...pageProps }/>
+        </ThemeProvider>
+    )
+
+}
